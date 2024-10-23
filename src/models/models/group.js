3a30f47181792aacc137a45f1deba2359d212bb3
Duc <thienductranhuu2784@gmail.com>
2024-10-23 12:07:22 -0400

    migrating models to db

--- src/models/models/group.js
+++ src/models/models/group.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Group.hasMany(models.User)
     }
   };
   Group.init({
--- src/models/models/groupRole.js
+++ src/models/models/groupRole.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class GroupRole extends Model {
+  class Group_User extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,12 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  GroupRole.init({
+  Group_User.init({
     groupId: DataTypes.INTEGER,
     roleId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'GroupRole',
+    modelName: 'Group_User',
   });
-  return GroupRole;
+  return Group_User;
 };
