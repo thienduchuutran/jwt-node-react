e6f4e3f22e661c645bfb9b5849bf56e901257ae3
Duc <thienductranhuu2784@gmail.com>
2024-10-23 13:42:31 -0400

    defining associations (relationships) between tables

--- src/models/models/user.js
+++ src/models/models/user.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.belongsTo(models.Group)
+      User.belongsToMany(models.Project, {through: 'Project_User'})
     }
   };
   User.init({
