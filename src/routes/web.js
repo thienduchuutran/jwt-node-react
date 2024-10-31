d8a36f4b835ab8eb614b904ca526e7a7094d42c5
Duc <thienductranhuu2784@gmail.com>
2024-10-31 19:49:50 -0400

    testing with restful API, controller returns data

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,7 @@
 //this is where we define the routes for the app
 import express from 'express'
 import homeController from '../controller/homeController'
+import apiController from '../controller/apiController'
 
 const router = express.Router()
 
@@ -14,6 +15,8 @@ const initWebRoutes = (app) => {
     router.post("/delete-user/:id", homeController.handleDeleteUser)
     router.get("/update-user/:id", homeController.getUpdateUserPage)  
     router.post("/user/update-user", homeController.handleUpdateUser)
+
+    router.get("/api/test-api", apiController.testApi)
 
     return app.use("/", router) //this is how we want our web to start with
 }
