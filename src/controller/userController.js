74ff61ee859c5d128abe256346c1044c44540b99
Duc <thienductranhuu2784@gmail.com>
2024-11-19 18:48:11 -0500

    setting up base for CRUD users

---
+++ src/controller/userController.js
@@ -0,0 +1,24 @@
+
+
+const readFunc = (req, res) => {
+
+}
+
+const createFunc = (req, res) => {
+    
+}
+
+const updateFunc = (req, res) => {
+    
+}
+
+const deleteFunc = (req, res) => {
+    
+}
+
+module.exports = {
+    readFunc,
+    createFunc,
+    updateFunc,
+    deleteFunc
+}
